553508561c41bd88fafc1c3e3ad5d14dc2f21b64
unknown <hiimprat@gmail.com>
2021-01-20 20:11:59 -0500

    Update possible positions UI

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,40 +1,118 @@
-import React, {useState} from 'react';
+import React, { useEffect,useContext,useState} from 'react';
 import '../index.css';
 import Knight from '../images/knight.svg'
 import Square from './square';
+import {Grid} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import KnightPlacedContext from '../contexts/KnightPlacedContext';
+import KnightPosContext from '../contexts/KnightPosContext';
+import DestinationSelContext from '../contexts/DestinationSelContext';
+import chessPositions from '../constants';
+import DestPosContext from '../contexts/DestPosContext';
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
 
 function Board(props){
-    
-    const board=[];
-    
-    
+    const forceUpdate = useForceUpdate();
+    const [board,setBoard]= useState([]);
+    const {placed, setPlaced} = useContext(KnightPlacedContext);
+    const {pos,setPos} = useContext(KnightPosContext);
+    const {destSelected,setDest} = useContext(DestinationSelContext);
+    const {destPos,setDestPos} = useContext(DestPosContext);
+    const [boardInitialized,setBoardInitialized]=useState(false);
     const isEven = (num)=>{
         return num%2==0;
     }
-    const placePiece = [];
 
-    for(let a =0;a<64;a++){
-        placePiece.push({});
-    }
-    
+    const possibleValueMoves=[];
+
+    const calc=()=>{
+        //calc possible positions
+        possiblePositions(pos);
+        console.log(board);
+        possibleValueMoves.forEach((val)=>{
+            //console.log(parseInt(val/8));
+            //console.log(board[(parseInt(val/8))].props.children[val%8]);
+            board[(parseInt(val/8))].props.children[val%8]=<Square value={val} color="square-selected " onClicked={onClickedFunc}></Square>
+            console.log(board[(parseInt(val/8))].props.children[val%8]);
+            
+        })
+        //board[(parseInt(pos/8))].props.children[pos%8]=<Square value={val} color="square-selected " style={backgroundImage: "url('"+Knight+"')"} onClicked={onClickedFunc}></Square>
+        let newBoard = [];
+        board.forEach((row) => {
+            newBoard.push(<div className="board-row">{[...row.props.children]}</div>);
+        });
+        setBoard(newBoard);
+        //forceUpdate();
+        console.log(board);
         
+    }
     
-    for(let i=0;i<8;i++){
-        const newRow=[]
-        for(let j=0;j<8;j++){
-            const color = (isEven(i) && isEven(j))||(!isEven(i)&&!isEven(j)) ? "light-square" : "dark-square";
-            newRow.push(<Square value={(i*8)+j} color={color}></Square>);
+    const possiblePositions=(pos)=>{
+        if(pos-17>0){
+            possibleValueMoves.push(pos-17);
         }
-        board.push(<div className="board-row">{newRow}</div>)
+        if(pos-15>0){
+            possibleValueMoves.push(pos-15);
+        }
+        if(pos-10>0){
+            possibleValueMoves.push(pos-10);
+        }
+        
+        if(pos-6>0){
+            possibleValueMoves.push(pos-6);
+        }
+
+        if(pos+6<63){
+            possibleValueMoves.push(pos+6);
+        }
+        if(pos+10<63){
+            possibleValueMoves.push(pos+10);
+        }
+        if(pos+15<63){
+            possibleValueMoves.push(pos+15);
+        }
+        if(pos+17<63){
+            possibleValueMoves.push(pos+17);
+        }
+        
+        console.log(possibleValueMoves);
+    }
+    const onClickedFunc=(value)=>{   
+        console.log(value);
 
     }
+    
+    useEffect(()=>{
+        if(!boardInitialized){
+            for(let i=0;i<8;i++){
+                const newRow=[]
+                for(let j=0;j<8;j++){
+                    const color = (isEven(i) && isEven(j))||(!isEven(i)&&!isEven(j)) ? "light-square " : "dark-square ";
+                    newRow.push(<Square value={(i*8)+j} color={color} onClicked={onClickedFunc}></Square>);
+                }
+                board.push(<div className="board-row">{newRow}</div>)
+                //console.log(board);
+            }
+            setBoardInitialized(true);
+            setBoard([...board]);
+        }
+        console.log("board was rerendered");
+        console.log(board);
+    })
+    
+    
     
     return(
-        <div>
-            <img src={Knight}/>
-            {board}
-            <Button variant="contained" >Calculate</Button>
+        <div align="center" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+            <div class="container">
+                {board}
+                <Button variant="contained" disabled={(destSelected && placed)?false:true} onClick={calc}>Calculate</Button>
+            </div>
+            
         </div>
     );
     
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -10,12 +10,12 @@ function Square(props){
     const value = props.value;
     const  {color} = props;
     const  [style,setStyle] = useState({});
+    //const [highlight,setHighlight] = useState({});
     const {placed, setPlaced} = useContext(KnightPlacedContext);
     const {pos,setPos} = useContext(KnightPosContext);
     const {destSelected,setDest} = useContext(DestinationSelContext);
     const {destPos,setDestPos} = useContext(DestPosContext);
-
-    const test = ()=>{
+    const selectSquare = ()=>{
         if(placed && value===pos){
             setStyle({}); 
             setPlaced(false);
@@ -29,18 +29,20 @@ function Square(props){
         else if(placed && !destSelected){
             setDestPos(value);
             setDest(true);
+            setStyle({opacity:'50%',backgroundColor:'red',borderColor:'blue',borderWidth:5});
         }
-        console.log("value: " + value);
-        console.log("pos: " + pos);
-        console.log("destPos: " + destPos);
-
+        else if(placed && destSelected){
+            
+        }
+        props.onClicked(props.value);
 
     }
+    const clicked = {opacity:'50%',backgroundColor:'purple'}
 
     return (
-        <button className={"square " + color}
-            onClick={test}
-            style={style}
+        <button className={"square " + color }
+            onClick={selectSquare}
+            style={style }
         >
         </button>
     );
